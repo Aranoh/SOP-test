47065f3fb93857d7a5b45f40f54316304583bcc9
Mark van Drimmelen <m.vandrimmelen@student.fontys.nl>
2017-03-08 10:33:40 +0100

    -JPA next gen

--- JEA62/src/main/java/util/InitUser.java
+++ JEA62/src/main/java/util/InitUser.java
@@ -5,9 +5,7 @@
  */
 package util;
 
-import dao.TweetDAO;
 import dao.UserDAO;
-import domain.Tweet;
 import domain.User;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -25,19 +23,24 @@ public class InitUser {
     @Inject
     UserDAO ud;
 
-    @Inject
-    TweetDAO td;
-
     @PostConstruct
     public void init() {
         User henk = new User("Henk");
         User piet = new User("Piet");
-        piet.FollowUser(henk);
         User bert = new User("Bert");
-        bert.FollowUser(henk);
         User klaas = new User("Klaas");
         User hans = new User("Hans");
         User sjaak = new User("Sjaak");
+
+        piet.FollowUser(henk);
+        bert.FollowUser(henk);
+
+        henk.CreateTweet("Hallo - " + henk.getUsername());
+        piet.CreateTweet("Hallo - " + piet.getUsername());
+        bert.CreateTweet("Hallo - " + bert.getUsername());
+        klaas.CreateTweet("Hallo - " + klaas.getUsername());
+        hans.CreateTweet("Hallo - " + hans.getUsername());
+        sjaak.CreateTweet("Hallo - " + sjaak.getUsername());
 
         ud.save(henk);
         ud.save(piet);
@@ -45,20 +48,6 @@ public class InitUser {
         ud.save(klaas);
         ud.save(hans);
         ud.save(sjaak);
-
-        Tweet hallo = new Tweet("Hallo - " + henk.getUsername(), henk);
-        Tweet hallo1 = new Tweet("Hallo - " + piet.getUsername(), piet);
-        Tweet hallo2 = new Tweet("Hallo - " + bert.getUsername(), bert);
-        Tweet hallo3 = new Tweet("Hallo - " + klaas.getUsername(), klaas);
-        Tweet hallo4 = new Tweet("Hallo - " + hans.getUsername(), hans);
-        Tweet hallo5 = new Tweet("Hallo - " + sjaak.getUsername(), sjaak);
-
-        td.save(hallo);
-        td.save(hallo1);
-        td.save(hallo2);
-        td.save(hallo3);
-        td.save(hallo4);
-        td.save(hallo5);
     }
 
 }
